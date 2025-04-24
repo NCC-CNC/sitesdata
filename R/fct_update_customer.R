6af761e19e965c1cdbdadd89269207a3fdb9d8d1
Dan Wismer <daniel.s.wismer@gmail.com>
2025-04-24 13:28:23 -0600

    fct: update dim_customer with user data

---
+++ R/fct_update_customer.R
@@ -0,0 +1,23 @@
+#' update_customer 
+#'
+#' @description A fct function
+#'
+#' @return The return value, if any, from executing the function.
+#'
+#' @noRd
+update_customer <- function(con, user_data_manager) {
+  DBI::dbExecute(con, "
+    UPDATE dim_customer
+    SET 
+      first_name = ?,
+      last_name = ?,
+      affiliation = ?
+    WHERE email = ?;
+  ", params = list(
+    user_data_manager()$first_name, 
+    user_data_manager()$last_name, 
+    user_data_manager()$affiliation, 
+    user_data_manager()$email
+    )
+  )
+}
