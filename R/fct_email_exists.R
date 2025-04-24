d8167168e6055d69a7ccdc72a5708982868df1ff
Dan Wismer <daniel.s.wismer@gmail.com>
2025-04-24 13:27:54 -0600

    fct: check if email exists in dim_customers

---
+++ R/fct_email_exists.R
@@ -0,0 +1,16 @@
+#' email_exists 
+#'
+#' @description Check if a customer email already exists in the dim_customer table.
+#'
+#' @return `TRUE` or `FALSE`
+#'
+#' @noRd
+email_exists <- function(con, email) {
+  result <- DBI::dbGetQuery(con, "
+    SELECT EXISTS (
+      SELECT 1 FROM dim_customer WHERE email = ?
+    ) AS email_found
+  ", params = list(email))
+  
+  return(as.logical(result$email_found))
+}
