bf78cd9b78070c91aa2af26ca9284c428810c5b5
Dan Wismer <daniel.s.wismer@gmail.com>
2025-04-24 13:30:02 -0600

    update or insert user data to dim_customer
    connect to db, and check if email exists in dim_customer. If not, insert user data. If it exists, update user data (mostly in case affiliation changed).

--- R/mod_submit.R
+++ R/mod_submit.R
@@ -17,9 +17,31 @@ mod_submit_ui <- function(id) {
 #' submit Server Functions
 #'
 #' @noRd 
-mod_submit_server <- function(id){
+mod_submit_server <- function(id, order_manager, user_data_manager){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
+    shiny::observeEvent(input$submit, {
+      
+      # connect to db
+      test_db_path <- "C:/Github/sitesdata-backend/test-db.sqlite"
+      con <- DBI::dbConnect(RSQLite::SQLite(), test_db_path)
+      
+      # DIM_CUSTOMER
+      ## check if email exists
+      if (email_exists(con, user_data_manager()$email)) {
+        ### update customer data
+        update_customer(con, user_data_manager) # fct_update_customer.R
+      } else {
+        ### insert new customer
+        DBI::dbWriteTable(con, "dim_customer", user_data_manager(), append = TRUE, row.names = FALSE)
+      }
+      
+      # DIM_ORDER
+      
+      # DIM_ORDERITEMS
+      
+      
+    })
  
   })
 }
