fff3bd4a9f950d7ad9e580e5deb6434f3855a87e
Dan Wismer <daniel.s.wismer@gmail.com>
2025-04-24 14:44:55 -0600

    insert into fact_order

--- R/mod_submit.R
+++ R/mod_submit.R
@@ -17,16 +17,18 @@ mod_submit_ui <- function(id) {
 #' submit Server Functions
 #'
 #' @noRd 
-mod_submit_server <- function(id, order_manager, user_data_manager){
+mod_submit_server <- function(id, order_manager, user_data_manager, geojson_aoi){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
     shiny::observeEvent(input$submit, {
+      
+      browser()
       
       # connect to db
       test_db_path <- "C:/Github/sitesdata-backend/test-db.sqlite"
       con <- DBI::dbConnect(RSQLite::SQLite(), test_db_path)
       
-      # DIM_CUSTOMER
+      # DIM_CUSTOMER ----
       ## check if email exists
       if (email_exists(con, user_data_manager()$email)) {
         ### update customer data
@@ -36,15 +38,22 @@ mod_submit_server <- function(id, order_manager, user_data_manager){
         DBI::dbWriteTable(con, "dim_customer", user_data_manager(), append = TRUE, row.names = FALSE)
       }
       
-      # DIM_ORDER
+      # DIM_ORDER ----
+      ## get customer_id
+      customer_id <- DBI::dbGetQuery(con, "
+      SELECT customer_id FROM dim_customer WHERE email = ?
+      ", params = list(user_data_manager()$email)
+      )
       
-      # DIM_ORDERITEMS
-      
-      
+      ## insert order
+      UTC <- as.character(lubridate::now(tzone = "UTC"))
+      DBI::dbExecute(con, "
+      INSERT INTO fact_order (customer_id, order_date, geojson_aoi)
+      VALUES (?, ?, ?);
+      ", params = list(as.numeric(customer_id), UTC, geojson_aoi())
+      )
     })
- 
-  })
-}
+})}
     
 ## To be copied in the UI
 # mod_submit_ui("submit_1")
