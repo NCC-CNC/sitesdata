bb5caeed7eb14f100a98c5be36b193c6b7f1bb65
Dan Wismer <daniel.s.wismer@gmail.com>
2025-05-12 15:30:15 -0600

    const.tif for counting # of planning units

--- R/app_global.R
+++ R/app_global.R
@@ -3,4 +3,7 @@ app_global <- quote({
   # product table
   product_df <- read.csv(system.file("app", "product.csv", package = "sitesdata"))
   
+  # NCC grid (all call values are 1)
+  ncc_1km <- terra::rast(system.file("app", "const.tif", package = "sitesdata"))
+  
 })
