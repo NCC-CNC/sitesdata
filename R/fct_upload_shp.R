ce65db7ca8dfbbe3f01156131d56991f93c32c91
Dan Wismer <daniel.s.wismer@gmail.com>
2025-05-13 12:06:19 -0600

    simplified upload shp. removed rested reactivity

--- R/fct_upload_shp.R
+++ R/fct_upload_shp.R
@@ -5,24 +5,24 @@
 #' @return The return value, if any, from executing the function.
 #'
 #' @noRd
-upload_shp <- function(userFile) {
+upload_shp <- function(user_files) {
+  # Get input file paths
+  infiles <- user_files$datapath
+  dir <- unique(dirname(infiles))
+  outfiles <- file.path(dir, user_files$name)
+  name <- strsplit(user_files$name[1], "\\.")[[1]][1]  # strip base name
   
-  if (is.null(userFile)){
+  # Move uploaded files to match original filenames
+  purrr::walk2(infiles, outfiles, ~file.rename(.x, .y))
+  
+  # Try reading the shapefile
+  shp_path <- file.path(dir, paste0(name, ".shp"))
+  x <- try(sf::read_sf(shp_path), silent = TRUE)
+  
+  # Return NULL on error
+  if (inherits(x, "try-error")) {
     return(NULL)
+  } else {
+    return(x)
   }
-
-  shp <- reactive({
-    if(!is.data.frame(userFile())) return()
-    
-    ## read as sf
-    infiles <- userFile()$datapath
-    dir <- unique(dirname(infiles))
-    outfiles <- file.path(dir, userFile()$name)
-    name <- strsplit(userFile()$name[1], "\\.")[[1]][1] # strip name
-    purrr::walk2(infiles, outfiles, ~file.rename(.x, .y))
-    x <- try(sf::read_sf(file.path(dir, paste0(name, ".shp"))))
-    if(any(class(x)=="try-error")) NULL else x
-  })
-    
-  return(shp)
 }
